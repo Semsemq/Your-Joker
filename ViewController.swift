6291cdb57af0bf31ce8b521f3435ff50b41936f6
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2023-07-02 12:46:02 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  your joker
+//
+//  Created by Sumayyah AHmad on 23/11/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+ func ViewDidAppear (_animated: Bool)  {
+            
+            print("viewWillappeare")
+            
+            
+            func ViewDidappear (_animated: Bool)  {
+                
+                
+                print("viewDidappeare")
+                
+                func  viewDidDisappear (_animated: Bool)  {
+                    
+                    print("viewDidDisappeare")
+                }
+                
+                
+                func textfield1(_animated: Bool)  {
+                }
+                
+                
+                func textfield(_animated: Any) {
+                }
+                
+                func button(_animated: Bool) {
+                }
+            }
+            
+            
+            
+            
+            struct
+            
+            ContentView_Previews {
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
